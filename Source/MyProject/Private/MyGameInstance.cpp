33bee92c0fffc1ceaf45c5dd0d0e2cb6bde62d2b
searoopy <searoopy@gmail.com>
2026-04-14 09:56:20 +0900

    set array

--- Source/MyProject/Private/MyGameInstance.cpp
+++ Source/MyProject/Private/MyGameInstance.cpp
@@ -8,6 +8,10 @@
 #include "Staff.h"
 #include "Person.h"
 
+#include "CourseInfo.h"
+#include "Algo/Accumulate.h"
+
+
 
 UMyGameInstance::UMyGameInstance()
 {
@@ -20,29 +24,88 @@ void UMyGameInstance::Init()
 	UE_LOG(LogTemp, Log, TEXT("====================="));
 	
 
-	//학사 정보 객체 생성
-	CourseInfo = NewObject<UCourseInfo>(this);
+
+	const int32 ArrayNum = 10;
+	TArray<int32> Int32Array;
 
 
-	//3명의 학생 추가.
-	UStudent* Student1 = NewObject<UStudent>(this);
-	Student1->SetName(TEXT("학생1"));
-	UStudent* Student2 = NewObject<UStudent>(this);
-	Student2->SetName(TEXT("학생2"));				
-	UStudent* Student3 = NewObject<UStudent>(this);	
-	Student3->SetName(TEXT("학생3"));
-
-	//알림에 구독.
-	CourseInfo->OnChanged.AddUObject(Student1, &UStudent::GetNotification);
-	CourseInfo->OnChanged.AddUObject(Student2, &UStudent::GetNotification);
-	CourseInfo->OnChanged.AddUObject(Student3, &UStudent::GetNotification);
+	for (int32 ix = 1; ix < ArrayNum; ix++)
+	{
+		Int32Array.Add(ix);
+	}
 
 
+	//짝수 제거.
+	Int32Array.RemoveAll(
+		//[]- 캡처 (외부내용을 람다안에서 사용할때 활용).
+		//() - 파라메터
+		//-> 반환형.
+		//{} 본문.
+		[](int32 Val) -> bool
+		{
+			return Val % 2 == 0;
+		}
+	);
 
-	//변경된 학사 정보 발행.
-	CourseInfo->ChangeCourseInfo(SchoolName, TEXT("학사 정보가 변경되었습니다."));
 
-	UE_LOG(LogTemp, Log, TEXT("====================="));
+	//짝수 삽입.
+	Int32Array += {2, 4, 6, 8, 10};
+
+
+
+	//비교(동등 비교).
+	TArray<int32> Int32ArrayCompare;
+	int32 CArray[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
+	Int32ArrayCompare.AddUninitialized(ArrayNum); //비교 실패를 위해 추가.
+
+
+	//c스타일배열을 TArray에 메모리 복사.
+	FMemory::Memcpy(Int32ArrayCompare.GetData(), CArray , sizeof(int32) * ArrayNum);
+
+
+	// 어서트 (크래시를 발생시키지 않고, 출력 로그 창에 오류 메시지 출력).
+	ensureAlways(Int32Array == Int32ArrayCompare);
+
+
+
+	//합계
+	int32 Sum = 0;
+	for(const int32& Val : Int32Array)
+	{
+		Sum += Val;
+	}
+	
+
+	//알고리즘 활용(합계 구하기).
+	int32 SumByAlgo = Algo::Accumulate(Int32Array, 0);
+
+	UE_LOG(LogTemp, Log, TEXT("sum= %d | SumByAlgo: %d "), Sum, SumByAlgo);
+
+
+
+	//tset 사용.
+	TSet<int32> Int32Set;
+	for( int32 ix = 1; ix < ArrayNum; ix++)
+	{
+		Int32Set.Add(ix);
+	}
+
+	//제거.
+	Int32Set.Remove(2);
+	Int32Set.Remove(4);
+	Int32Set.Remove(6);
+	Int32Set.Remove(8);
+	Int32Set.Remove(10);
+
+
+	//추가.
+	Int32Set.Add(2);
+	Int32Set.Add(4);
+	Int32Set.Add(6);
+	Int32Set.Add(8);
+	Int32Set.Add(10);
+
+	
 
 }
  
